5fa426a04efc3985abcad4da82ab865f81470bc6
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-03 16:24:59 -0800

    fixed things?

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if last_digit > 5
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit)
-if last_digit == 0
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
-if last_digit < 6 and last_digit != 0
-    print("Last digit of {} is {} and is less than 6 and is not 0".format(number, last_digit))
+ld = number % 10
+if ld > 5:
+    print("Last digit of %d is %d and is greater than 5" % number, ld);
+if ld == 0:
+    print("Last digit of %d is %d and is 0" % number, ld);
+if ld < 6 and ld != 0:
+    print("Last digit of %d is %d and is less than 6 and is not 0" % number, ld);
