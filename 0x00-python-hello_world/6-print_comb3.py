4345b09bdbc31affe18dbf8e096667c6e5754997
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-03 18:36:51 -0800

    asdf

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def comb(L):
-
-    for i in range(0, 9):
-        for j in range(0, 9):
-                if i != j:
-                    print(L[i], L[j])
+for i in range(0, 9):
+    for j in range(0, 9):
+        if i != j:
+            print("{}{}".format(i, j), end=", ")
