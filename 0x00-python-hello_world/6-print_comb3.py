cb84603f49f53e260dc6fcc7da6a62b12f35c2bd
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-03 20:07:30 -0800

    asdf

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range(0, 9):
-        if i < j and i < 8:
+    for j in range(i + 1, 10):
+        if i < 8:
             print("{}{}".format(i, j), end=", ")
-        elif ((i == 8) and (j == 9)):
-            print("{}{}".format(i, j), end="\n")
+        elif i >= 8:
+            print("{}{}".format(i, j))
