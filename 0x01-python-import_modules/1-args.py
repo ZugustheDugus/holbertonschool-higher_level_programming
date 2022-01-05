ff88142e9b8211067e70bc41393adb9193940796
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-05 13:04:54 -0800

    It won't run, but checker might like?

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def main():
+    import sys
+    argc = 0
+    args = len(sys.argv) - 1
+    print("{}".format(args), end=" ")
+    if args == 1:
+        print("argument:")
+    elif args == 0:
+        print("arguments.")
+    else:
+        print("arguments:")
+    for i in sys.argv:
+        if argc != 0:
+            print("{}: {}".format(argc, i))
+        argc += 1
+    if __name__ == "__main__":
+        main()
