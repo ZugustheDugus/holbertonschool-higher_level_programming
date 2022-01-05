ba96826d5f1dd9fc97dc3156b03b45af32cbc5f1
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-05 13:12:48 -0800

    executed as script?

--- 0x01-python-import_modules/2-variable_load.py
+++ 0x01-python-import_modules/2-variable_load.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-from variable_load_2 import a
-print("{}".format(a))
+def main():
+    from variable_load_2 import a
+    print("{}".format(a))
+if __name__ == "__main__":
+    main()
