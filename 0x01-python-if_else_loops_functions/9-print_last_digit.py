cb848034626ec5cb70a63235d1a47b506b7a6e9b
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-04 17:41:08 -0800

    asdf

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    ld = abs(number) % 10
+    if number < 0:
+        number *= -1
+    ld = number % 10
     print("{}".format(ld), end="")
+    return(ld)
