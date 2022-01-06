4be3d77bcd7e51aaab4381a66f5b9463e8e1e5c5
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-06 15:37:51 -0800

    asdf

---
+++ 0x02-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in range(0, len(my_string)):
+        if my_string[i] == chr(67):
+            continue
+        if my_string[i] == chr(99):
+            continue
+        new_string = new_string + my_string[i]
+    return new_string
