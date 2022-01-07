7e77f9d7180cdfd64d34f9075d63f65e323cf353
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-06 16:54:51 -0800

    asdf

---
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    list1 = [0, 0]
+    for i in range(2):
+        if i + 1 <= len(tuple_a):
+            list1[i] += tuple_a[i]
+        if i + 1 <= len(tuple_b):
+            list1[i] += tuple_b[i]
+        new_tup = (list1[0], list1[1])
+        return new_tup
