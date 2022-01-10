fab259bed8da6438973129c5736cdebc430f04dd
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-10 13:36:00 -0800

    task 0

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square = [[0 for a in range(len(matrix))] for b in range(len(matrix[0]))]
+    square = [[0 for a in range(len(matrix[0]))] for b in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             square[i][j] = (matrix[i][j])**2
