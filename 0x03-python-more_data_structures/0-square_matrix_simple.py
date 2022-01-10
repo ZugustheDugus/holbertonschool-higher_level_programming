d0cf87fccca3afa7dad24856cbafabddfafcde1e
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-10 13:31:12 -0800

    task 0

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
+    sqr_mtrx = [[0 for a in range(len(matrix))] for b in range(len(matrix[0]))]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            new_matrix[i][j] = (matrix[i][j])**2
-    return new_matrix
+            sqr_mtrx[i][j] = matrix[i][j]**2
+    return sqr_mtrx
