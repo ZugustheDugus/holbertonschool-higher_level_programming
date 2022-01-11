4c570c78161ccaa7e4600c6a89b0c8b221922222
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-10 19:01:20 -0800

    missing a few values?

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'IX': 9, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'D': 500, 'CM': 900, 'M': 1000}
+    rome_dict = {'I': 1, 'IV': 4, 'V': 5, 'VI': 6, 'X': 10, 'IX': 9, 'XI': 11, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'D': 500, 'CM': 900, 'M': 1000}
     result = 0
     if roman_string in rome_dict:
         result = rome_dict[roman_string]
