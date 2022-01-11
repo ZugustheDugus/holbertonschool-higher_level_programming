34eb3ecc9c790f992d954984e8c0c37a22efb930
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-10 18:57:58 -0800

    found the typo

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -3,14 +3,11 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'IX': 9, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100 'D': 500, 'CM': 900, 'M': 1000}
+    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'IX': 9, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'D': 500, 'CM': 900, 'M': 1000}
     result = 0
-
     if roman_string in rome_dict:
         result = rome_dict[roman_string]
         return result
-    
     for i in roman_string:
         result += rome_dict[i]
-
-        return result
+    return result
