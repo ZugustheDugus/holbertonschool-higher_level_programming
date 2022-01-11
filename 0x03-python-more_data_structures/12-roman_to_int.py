275ec5b040e3922cf2363caafa89db6cccd0a15e
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-10 19:18:03 -0800

    asdf

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
-
-    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    dec, num = 0, 0
-
-    for i in range(len(roman_string) - 1, -1, -1):
-        if rome_dict[roman_string[i]]:
-            num += rome_dict[roman_string[i]]
-        else:
-            num -= rome_dict[roman_string[i]]
-        dec = rome_dict[roman_string[i]]
-    return num
+    dec, int = 0, 0
+    if roman_string and type(roman_string) is str:
+        rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for i in range(len(roman_string) - 1, -1, -1):
+            if rome_dict[roman_string[i]] >= dec:
+                int += rome_dict[roman_string[i]]
+            else:
+                int -= rome_dict[roman_string[i]]
+            dec = rome_dict[roman_string[i]]
+        return int
+    return int
