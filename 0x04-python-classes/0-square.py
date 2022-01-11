368ba4d93a5e8bd3213ba731fd73c1aa85a4f052
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-11 12:52:44 -0800

    documented

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"Class containing attributes for a square"
 class Square():
+    "Square only has a private size attribute"
     def __init__(self, size):
         self.__size = size
