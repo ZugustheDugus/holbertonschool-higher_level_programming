edd079021c410316663c88ef2a7b48963480ed36
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-11 13:27:46 -0800

    asdf

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"Class for a square with attributes"
+
+
+class Square:
+    "Attributes for the square"
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        "area of the square"
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = value
