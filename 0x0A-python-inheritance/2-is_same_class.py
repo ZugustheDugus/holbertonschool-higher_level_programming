dfee0fbf94b4f7552322554c353666e305c5a722
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 14:51:20 -0800

    task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Function that identifies if two classes are the same
+"""
+
 def is_same_class(obj, a_class):
-    
+"""
+Return true if same, else return false
+"""
+return(type(obj) == a_class)
