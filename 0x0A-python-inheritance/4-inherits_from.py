664a0e43e3d97c83482a6eeaeb23ab8dd90b0a05
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 15:42:31 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Function for obj to inherit methods from a_class
+"""
 def inherits_from(obj, a_class):
-    
+    """
+    Return true if obj inherits from a_class
+    else return false
+    """
+    if type(obj) == a_class:
+        return False
+    return(issubclass(type(obj), a_class))
