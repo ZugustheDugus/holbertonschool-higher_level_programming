e032e6b1ba8cd9985bd7facb6bc5f9e058ea88ab
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 15:55:55 -0800

    added second argument

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,9 +2,11 @@
 """
 Function to determine if something is a class
 """
+
+
 def is_kind_of_class(obj, a_class):
     """
     Return true if object is an instance of or inherited from a_class
     else return false
     """
-    return(isinstance(obj) == a_class)
+    return(isinstance(obj, a_class))
