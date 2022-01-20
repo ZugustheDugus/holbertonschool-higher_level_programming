09378c3ff1d6d033dde5c1c3ea16cc39c74b7d20
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 15:57:07 -0800

    fixed indentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@ Function that identifies if two classes are the same
 """
 
 def is_same_class(obj, a_class):
-"""
-Return true if same, else return false
-"""
-return(type(obj) == a_class)
+    """
+    Return true if same, else return false
+    """
+    return(type(obj) == a_class)
