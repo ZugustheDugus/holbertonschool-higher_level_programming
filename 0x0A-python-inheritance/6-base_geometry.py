483cfcda06dd248c657c6c597a446e59d75cd1b1
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 15:51:55 -0800

    task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+Base Geometry class, but with an placeholder for area
+"""
+
+
+class BaseGeometry:
+    "Area placeholder"
+    def area(self):
+        raise Exception("area() is not implemented")
