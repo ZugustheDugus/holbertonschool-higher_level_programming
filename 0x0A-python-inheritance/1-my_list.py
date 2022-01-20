78ccdbe86fe02e53192b65fecd3a9d76f38f7a30
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 13:59:18 -0800

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-def print_sorted(self):
+"""
+Class for My List
+"""
+
+
+class MyList(list):
+    """
+    inherit from list
+    """
+    def __init__(self):
+        "initialize the object"
+        super().__init__()
+
+    def print_sorted(self):
+        "Print the class sorted"
+        print(sorted(self))
