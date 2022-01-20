31ee6957b3a5d9a15c2cdc14ff2b11e184cd864a
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 13:22:10 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+Lookup the methods of an object
+"""
 def lookup(obj):
-    
+    """
+    Return the list of attributes and methods of the object
+    """
+    return dir(obj)
