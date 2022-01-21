2a3b2aa1392e512c57b067de006211b3cafaee99
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 14:25:01 -0800

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,6 +3,8 @@
 Function to return JSON representation of I/O string
 """
 
+import json
 
 def to_json_string(my_obj):
-    
+    """return JSON representation of object"""
+    return json.dumps(my_obj)
