51cf273c199eb8eea6232b0220aa46b115e3da90
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 14:27:38 -0800

    task 3

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Function to convert obj from JSON to normal
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Convert from JSON"""
+    return json.loads(my_str)
