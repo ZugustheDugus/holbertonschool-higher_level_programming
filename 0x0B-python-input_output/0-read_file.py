c9b21ca1e7a90cc62d1b61cf81e169fcb1eee752
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 12:45:30 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,7 @@ I/O Function that reads a file
 
 
 def read_file(filename=""):
-    
+    """read a file"""
+    with open("my_file_0.txt", encoding="utf-8") as UTF8:
+        text = UTF8.read()
+        print("{}".format(text))
