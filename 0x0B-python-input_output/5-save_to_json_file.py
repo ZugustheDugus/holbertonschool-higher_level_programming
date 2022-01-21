70d89c3e1b12cb953206cd5fca10b97592a7adcf
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 14:33:06 -0800

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+save data to a JSON file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save my_obj to filename in JSON"""
+    with open(filename, "w+", encoding="utf-8") as JSON:
+        return json.dump(my_obj, JSON)
