99cee734abc933ad6187e5d90a043e81171072c7
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 13:36:52 -0800

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,6 @@ I/O Function that appends to a file
 
 
 def append_write(filename="", text=""):
-    
+    """write and then append to file"""
+    with open("file_append.txt", mode="a+", encoding="utf-8") as UTF8:
+        return(UTF8.write(text))
