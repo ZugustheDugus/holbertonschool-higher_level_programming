bc5ef8e083af8fed2ab322002dd504cd2b4053f7
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 13:31:47 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,6 @@ I/O Function that writes a file
 
 
 def write_file(filename="", text=""):
-    
+    """ Write to a file """
+    with open("my_first_file", mode="w+", encoding="utf-8") as UTF8:
+        return(UTF8.write(text))
