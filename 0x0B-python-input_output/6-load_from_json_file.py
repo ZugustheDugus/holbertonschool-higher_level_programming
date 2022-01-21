7d47a75b2d2d82db76dd0ee0d86c0f01b55d9d1a
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 14:39:27 -0800

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Load from a JSON obj
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """load from filename"""
+    with open(filename, mode="r", encoding="utf-8") as LOAD:
+        return json.load(LOAD)
