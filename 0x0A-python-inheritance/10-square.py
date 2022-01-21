edc353839edc7ff38b3ec13ce2e940298b5ae46c
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-20 19:32:13 -0800

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Square subclass that inherits from Rectangle and Base Geometry
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    "Square Subclass, inheriting from Rectangle"
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+    
