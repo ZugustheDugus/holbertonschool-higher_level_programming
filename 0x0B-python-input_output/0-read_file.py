71d52328785e8b4120e0d723e0e9a7e0ba9e9eab
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 13:46:38 -0800

    added no ending space to print command

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ I/O Function that reads a file
 def read_file(filename=""):
     """read a file and print"""
     with open(filename, encoding="utf-8") as UTF8:
-        print(UTF8.read())
+        print(UTF8.read(), end="")
