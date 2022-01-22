7fece7fadaa7843af6b535f906a527581ea51c1a
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 20:42:11 -0800

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Return list of pascal's triangle
+    """
+    triangle = []
+    for i in range(n):
+        triangle.append([])
+        for j in range(i + 1):
+            if j == i or j == 0:
+                triangle.append(1)
+            else:
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        return triangle
