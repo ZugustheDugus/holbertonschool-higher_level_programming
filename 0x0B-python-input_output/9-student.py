5f07a77ac7782376e51dadd65d10098af3c96e14
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 20:27:51 -0800

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Class for Student that defines by pbulc firstname, listname, age
+"""
+
+
+class Student():
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dictionary representation of instance of Student Class"""
+        return vars(self)
