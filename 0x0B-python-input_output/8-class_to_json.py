aaea55a67d213fc75d6f72efe5564c244046fa0d
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 20:22:48 -0800

    tsak 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+Return dictionary description with sample data structure
+"""
+
+def calss_to_json(obj):
+    """
+    Return dictionary description with simple data structure
+    """
+    return vars(obj)
