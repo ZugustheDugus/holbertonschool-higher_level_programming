5f1bc5d05b9d79c6ed0df1c3bc1fdb14cffa77d9
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 20:34:50 -0800

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Student class defined by public first_name, last_name, age)
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary representatin of Student instance"""
+        if attrs is none:
+            return vars(self)
+        neu_dict = {}
+        for i in attrs:
+            try:
+                neu_dict[i] = cars(self)[i]
+            except Exception:
+                pass
+        return neu_dict
