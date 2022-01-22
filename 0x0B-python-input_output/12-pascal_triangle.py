072a6a80476208596447d064ebf90237aa9f42e8
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 20:44:17 -0800

    wrong spacing

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,4 +16,4 @@ def pascal_triangle(n):
                 triangle.append(1)
             else:
                 triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        return triangle
+    return triangle
