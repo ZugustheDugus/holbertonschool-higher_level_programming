5ae138c3e4750cc446f57c8761c1ce1be180435b
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-21 20:48:09 -0800

    asdf

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,12 +8,16 @@ def pascal_triangle(n):
     """
     Return list of pascal's triangle
     """
-    triangle = []
+    pas_list = []
+    if n <= 0:
+        return pas_list
+    list = [1]
     for i in range(n):
-        triangle.append([])
-        for j in range(i + 1):
-            if j == i or j == 0:
-                triangle.append(1)
-            else:
-                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-    return triangle
+        pas_list.append(list)
+        new_list = []
+        new_list.append(list[0])
+        for i in range(len(list) - 1):
+            new_list.append(list[i] + list[i + 1])
+        new_list.append(list[-1])
+        list = new_list
+    return pas_list
