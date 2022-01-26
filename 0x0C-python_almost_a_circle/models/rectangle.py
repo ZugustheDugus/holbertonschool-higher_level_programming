4db00977c8cef8af8219ab2ac418fd787c9547c3
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-26 13:23:47 -0800

    fixed some typos and added values to rectangle

--- 0x0C-python_almost_a_circle/models/rectangle.py
+++ 0x0C-python_almost_a_circle/models/rectangle.py
@@ -6,6 +6,10 @@ Rectangle that inherits from Base class
 
 from models.base import Base
 
-class Rectangle(BaseGeometry):
+class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
-        pass
+        width = self.__width
+        height = self.__height
+        x = self.__x
+        y = self.__y
+        super().__init__(Rectangle)
