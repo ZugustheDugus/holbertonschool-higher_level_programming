d2ccb48891e0adf0ab581025b15d42482e651b8e
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-26 13:33:25 -0800

    updated rectangle with properties

--- 0x0C-python_almost_a_circle/models/rectangle.py
+++ 0x0C-python_almost_a_circle/models/rectangle.py
@@ -12,4 +12,31 @@ class Rectangle(Base):
         height = self.__height
         x = self.__x
         y = self.__y
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         super().__init__(Rectangle)
+
+    @property
+    def height(self):
+        "Rectangle height"
+        return self.__height
+
+    @property
+    def width(self):
+        "Rectangle width"
+        return self.__width
+
+    @property
+    def area(self):
+        "Rectangle area"
+        return self.__width * self.__height
