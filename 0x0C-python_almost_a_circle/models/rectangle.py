6d3622744a242cb68d1c1422e24efca2e24cc353
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-27 12:42:43 -0800

    updated rectangle

--- 0x0C-python_almost_a_circle/models/rectangle.py
+++ 0x0C-python_almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ from models.base import Base
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
+        id = self.__id
         width = self.__width
         height = self.__height
         x = self.__x
@@ -36,7 +37,28 @@ class Rectangle(Base):
         "Rectangle width"
         return self.__width
 
-    @property
     def area(self):
         "Rectangle area"
         return self.__width * self.__height
+
+    def display(self):
+        "Prints rectangle dimensions in #'s"
+         string = ""
+         if self.__width != 0 and self.__height != 0:
+             for i in range(self.height):
+                 for j in range(self.width):
+                     string += '#'
+                     if i != self.__height - 1:
+                         string += '\n'
+        return string
+
+    def __str__(self):
+        "__str__ method of the Rectangle"
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.__area, id, self.__x, self.__y, self.__height, self.__width))
+    def update(self, *args, **kwargs):
+        "assigns each element to an arg"
+        args = (self.__id, self.__x, self.__y, self.__width, self.__height)
+        update(*args)
+        kwargs = {self.__id, self.__x, self.__y, self.__width, self.__height}
+
+    
