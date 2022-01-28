e867d2ca14b98a209d6d124b3c28c836cb367288
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-27 21:26:24 -0800

    update

--- 0x0C-python_almost_a_circle/models/square.py
+++ 0x0C-python_almost_a_circle/models/square.py
@@ -2,10 +2,33 @@
 """
 Square class which draws from Base
 """
+from models.rectangle import Rectangle
 
 
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        id = self.__id
+        size = self.__size
+        x = self.__x
+        y = self.__y
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        super().__init__(Square)
 
+    def update(self, *args, **kwargs):
+        "assigns each element to an arg"
+        args = (self.__id, self.__x, self.__y, self.__size)
+        update(*args)
+        kwargs = {self.__id, self.__x, self.__y, self.__size}
 
-def update(self, *args, **kwargs):
+    def to_dictionary(self):
+        Dict = {self.__id, self.__size, self.__x, self.__y}
 
-def to_dictionary(self):
+    def __repr__(self):
+        return "Square({:d}, {:d})".format(self.__width, self.__height)
