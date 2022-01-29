5c7f1143e20f7e0dc4353bde3c0ef5d96c3cb19d
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-28 16:59:41 -0800

    adding in __init__.py

--- 0x0C-python_almost_a_circle/models/base.py
+++ 0x0C-python_almost_a_circle/models/base.py
@@ -25,9 +25,7 @@ class Base:
 
         @staticmethod
         def to_json_string(list_dictionaries):
-            if list_dictionaries is None:
-                return "[]"
-            if len(list_dictionaries) == 0:
+            if list_dictionaries is None or list_dictionaries == []:
                 return "[]"
             return json.dumps(list_dictionaries)
 
