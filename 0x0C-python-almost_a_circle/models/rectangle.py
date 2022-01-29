5e3adb5b3c8c6cd62ea9447a2745bb36f74e4823
ZugustheDugus <h1cooper2019@protonmail.com>
2022-01-28 18:46:28 -0800

    sanity check

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,42 +1,84 @@
 #!/usr/bin/python3
-"""Tasks 2 - 9
+"""
+Rectangle that inherits from Base class
 """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class inheriting from Base
+    """
+    Rectangle class inheriting from Basse class and validating
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Check for correct initialization input"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
         if type(height) is not int:
             raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
         if type(x) is not int:
             raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
         if type(y) is not int:
             raise TypeError("y must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         if y < 0:
             raise ValueError("y must be >= 0")
         super().__init__(id)
+        self.__id = id
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
 
+    @property
+    def height(self):
+        """Rectangle height getter"""
+        return self.__height
+
+    @property
+    def width(self):
+        """Rectangle width getter"""
+        return self.__width
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        self.__y = value
+
     def area(self):
-        """Returns the area of the rectangle"""
-        return self.__height * self.__width
+        """Rectangle area"""
+        return self.__width * self.__height
 
     def display(self):
-        """Prints out a rectangle made of #"""
+        """Prints rectangle dimensions in #s"""
         print("\n" * self.__y, end="")
         for i in range(self.__height):
             print(" " * self.__x, end="")
@@ -48,82 +90,16 @@ class Rectangle(Base):
             format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
-        """Updates an object's attributes"""
-        flag = 0
-        if args is not None:
-            flag = 1
-        if len(args) > 0 and flag == 1:
-            self.id = args[0]
-            if len(args) >= 2:
-                self.__width = args[1]
-            if len(args) >= 3:
-                self.__height = args[2]
-            if len(args) >= 4:
-                self.__x = args[3]
-            if len(args) >= 5:
-                self.__y = args[4]
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        """update object attributes"""
+        attrs, i = ['id', 'width', 'height', 'x', 'y'], 0
+        for value in args:
+            setattr(self, attrs[i], value)
+            i += 1
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def to_dictionary(self):
-        """Returns a dictionary representation of the rectangle's attributes"""
-        atts = {'id': self.id, 'width': self.width, 'height': self.height,
+        """return a dictionary of  attributes"""
+        Dict = {'id': self.id, 'width': self.width, 'height': self.height,
                 'x': self.x, 'y': self.y}
-        return atts
-
-    @property
-    def width(self):
-        """Getter for width"""
-        return self.__width
-
-    @property
-    def height(self):
-        """Getter for height"""
-        return self.__height
-
-    @property
-    def x(self):
-        """Getter for x"""
-        return self.__x
-
-    @property
-    def y(self):
-        """Getter for x"""
-        return self.__y
-
-    @width.setter
-    def width(self, value):
-        """Setter for width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @height.setter
-    def height(self, value):
-        """Setter for height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @x.setter
-    def x(self, value):
-        """Setter for x"""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @y.setter
-    def y(self, value):
-        """Setter for y"""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
+        return Dict
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,51 +1,43 @@
 #!/usr/bin/python3
-"""Tasks 10 -
+"""
+Square class which inherits from Base
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Creates a Square based off the Rectangle class
+    """
+    Square class, inheriting its attrs from Base
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes based off Rectangle"""
+        """init the square based on Rectangle"""
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """Print method"""
-        return "[Square] ({}) {}/{} - {}".\
-            format(self.id, self.x, self.y, self.width)
+    def to_dictionary(self):
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
 
     def update(self, *args, **kwargs):
-        """Update method for the object's attributes"""
-        flag = 0
-        if args is not None:
-            flag = 1
-        if len(args) > 0 and flag == 1:
-            self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-        else:
+        """Update args for the class"""
+        attrs, i = ['id', 'size', 'x', 'y'], 0
+        if args:
+            for value in args:
+                setattr(self, attrs[i], value)
+                i += 1
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-    def to_dictionary(self):
-        """Returns a dictionary representation of the square's attributes"""
-        atts = {'id': self.id, 'size': self.size,
-                'x': self.x, 'y': self.y}
-        return atts
+    def __str__(self):
+        """method for printing the square class"""
+        return "[Square] ({}) {}/{} - {}".\
+            format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Getter"""
-        return super().width
+        """getter for size"""
+        return self.width
 
     @size.setter
     def size(self, value):
-        """Setter"""
+        """Setter for the size"""
         self.width = value
         self.height = value
