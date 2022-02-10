ec52d1e149490a75e4aead3db7e2d252fd2f428a
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-09 16:39:51 -0800

    fixed spacing

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- Task 9
 -- MySQL script to create a second table
 CREATE TABLE [IF NOT EXISTS] first_table (
-       id INT,
-       name VARCHAR(256),
-       score INT
+    id INT,
+    name VARCHAR(256),
+    score INT
 );
 INSERT INTO second_table VALUES (1, "John", 10);
 INSERT INTO second_table VALUES (2, "Alex", 3);
