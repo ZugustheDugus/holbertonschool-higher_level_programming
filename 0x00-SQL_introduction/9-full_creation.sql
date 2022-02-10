d05b4f88093ee44d2b8ef62ab48a55004c024acd
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-09 16:49:34 -0800

    asdf

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,9 @@
 -- Task 9
 -- MySQL script to create a second table
-CREATE TABLE [IF NOT EXISTS] first_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-INSERT INTO second_table VALUES (1, "John", 10);
-INSERT INTO second_table VALUES (2, "Alex", 3);
-INSERT INTO second_table VALUES (3, "Bob", 14);
-INSERT INTO second_table VALUES (4, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUE
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
