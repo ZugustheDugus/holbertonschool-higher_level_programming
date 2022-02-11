3e0cfdcf4096d4a5f04dc73f3dde7756d55206b3
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-10 16:44:01 -0800

    task 2

---
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Task 2
+-- MySQL script to create a user with access to a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY "user_0d_2_pwd";
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
