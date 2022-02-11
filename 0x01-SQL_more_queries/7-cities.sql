12b8baa158c4461a830fee359bd0c20edd3510b4
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-10 17:49:49 -0800

    asdf

---
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Task 7
+-- MySQL script to create a database of cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+       id INT NOT NULL ATUO_INCREMENT PRIMARY KEY (id),
+       state_id INT NOT NULL FOREIGN KEY (id),
+       name VARCHAR(256) NOT NULL
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id)
+);
