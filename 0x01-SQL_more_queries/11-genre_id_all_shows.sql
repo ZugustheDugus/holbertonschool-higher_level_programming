5188560ab9c51ba35bb3c24cce9d31c465d5f928
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-10 18:27:06 -0800

    asdf

---
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Task 11
+-- MySQL script to show all contained in a database of shows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title. ASC, tv_show_genres.genre_id ASC;
