c8dba0d2a8a9e44e4a6b1b120d4991372f0d1274
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-10 16:40:35 -0800

    task 1

---
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Task 1
+-- MySQL script to create a user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost';
