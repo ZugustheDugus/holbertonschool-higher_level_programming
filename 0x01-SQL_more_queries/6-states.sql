27c90a618c4f5c604c323fd05b02b27de2ceeef5
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-10 17:47:39 -0800

    task 6

---
+++ 0x01-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Task 6
+-- MySQL script to create a database containing US states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states (
+       id INT NOT NULL AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id)
+);
