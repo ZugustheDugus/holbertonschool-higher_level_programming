7486cfb24b03e2a91175d2b2d5bab145ca8915ba
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-10 17:06:05 -0800

    fixed missing comma

--- 0x01-SQL_more_queries/5-unique_id.sql
+++ 0x01-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Task 5
 -- MySQL script to create a unique_id table
 CREATE TABLE IF NOT EXISTS unique_id (
-       id INT DEFAULT 1 UNIQUE
+       id INT DEFAULT 1 UNIQUE,
        name VARCHAR(256)
 );
