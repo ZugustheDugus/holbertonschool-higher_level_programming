f81154a7be0dcdb3750f4b6a5da78e41bcfbadc0
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-02-23 12:16:06 -0600

    asdf

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Lists all states from a database hbtn_0e_0_usa
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                        port = 3306,
+                        user=argv[1],
+                        passwd=argv[2],
+                        db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    
+    cur.close()
+    db.close()
