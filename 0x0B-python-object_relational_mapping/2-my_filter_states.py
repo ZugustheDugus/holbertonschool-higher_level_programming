655208dd0d7cf9844c5255b3a5c8f8424391db48
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-23 12:23:28 -0800

    asdf

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ def match_state():
                          passwd=sys.argv[2], database=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s", [format(sys.argv[4])])
+    cursor.execute("""SELECT * FROM states WHERE name LIKE %s
+    ORDER BY states.id ASC""", [format(sys.argv[4])])
     rows = cursor.fetchall()
     for row in rows:
         print(row)
