f42ca6036ec41ddc23cb1e6bcf88de0fa4b9ee1c
ZugustheDugus <h1cooper2019@protonmail.com>
2022-02-23 12:07:29 -0800

    asdf

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -4,17 +4,18 @@ Display all states which match an argument from db hbtn_0e_0_usa
 """
 
 import MySQLdb
+import sys
 
 
 def match_state():
-    from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], database=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (argv[4],))
-    for rows in cursor.fetchall():
-        print(rows)
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", [format(sys.argv[4])])
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
     
     cursor.close()
     db.close()
