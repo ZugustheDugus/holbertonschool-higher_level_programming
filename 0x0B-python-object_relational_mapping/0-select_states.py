30df881ea84bd4bc6bfa33dc13a3faefb4c10743
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-02-23 12:19:59 -0600

    states.id, not id

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -8,14 +8,11 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost",
-                        port = 3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+    db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1],
+                        passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
