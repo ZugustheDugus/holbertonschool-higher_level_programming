f652b09945bce9fe9547abb2f905a260c1e3e8f7
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-02-23 12:22:14 -0600

    utf-8 needed?

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(host="localhost", port = 3306, user=argv[1],
-                        passwd=argv[2], db=argv[3])
+                        passwd=argv[2], db=argv[3], charset="utf-8")
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
