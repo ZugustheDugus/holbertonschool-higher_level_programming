c0e56d94351982bb629d4ed2ed1c2438f0e878f1
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-02-23 12:55:29 -0600

    asdf

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa """
+"""
+list all states from database hbtn_0e_0_usa
+"""
 
-if __name__ == '__main__':
+import MySQLdb
 
-    import MySQLdb
-    import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+def prnt_state():
+    """Prints the states the states sorted by id, ascending"""
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                        passwd=argv[2], database=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states")
+    for rows in cursor.fetchall():
+        print(rows)
+
+    cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    prnt_state()
