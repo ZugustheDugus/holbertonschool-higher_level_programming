1fd433b36bab5c94fa79d723312aba09bcb73e5d
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-03-08 16:55:16 -0600

    task 2

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+"""
+Take URL and email and send a post request to the URL
+with email as the parameter
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    url = sys.argv[1]
+    val = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(val)
+    data = data.encode('UTF-8')
+    rqst = urllib.request.Request(url, data)
+    with urllib.request.urlopen(rqst) as response:
+        _page = response.read()
+
+    print(_page.decode('UTF-8'))
