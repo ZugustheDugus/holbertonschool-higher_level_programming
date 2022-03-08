904172d32f4827a280dc28718eff740aac81e93f
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-03-08 17:24:10 -0600

    task 3

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script to catch HTTP errors
+"""
+
+
+from cmath import e
+
+
+if __name__ == '__main__':
+
+    import sys
+    import urllib.request
+    import urllib.error
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+            print(html.decode('UTF-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
