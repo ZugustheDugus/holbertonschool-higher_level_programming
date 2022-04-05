53c6a835fe23e6cd397a7d8b2b10a4add7f63539
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-04 21:23:08 -0500

    task 11

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length < 4) {
-    console.log('0');
+  console.log('0');
 } else {
-    const x = process.argv.slice(2);
-    x.sort((a, b) => b - a);
-    console.log(x[1]);
+  const x = process.argv.slice(2);
+  x.sort((a, b) => b - a);
+  console.log(x[1]);
 }
