24ae54ecbdb73738ee4f52a27cd04a0256cd6c8d
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-04 20:28:00 -0500

    semistandard

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let i = 0;
 while (i < strings.length) {
-    console.log(strings[i]);
-    i++
+  console.log(strings[i]);
+  i++;
 }
