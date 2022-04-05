72bad4bd74204b09b88ef0c8f7dbfa14ccb7604e
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-04 21:09:16 -0500

    push to machine

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+function fctrl(a) {
+    if (isNaN(a)) {
+        return 1;
+    }
+    if (a < 0) {
+        return -1;
+    }
+    if (a === 0) {
+        return 1;
+    } else {
+        return a * fctrl(a - 1);
+    }
+}
+console.log(fctrl(process.argv[2]));
