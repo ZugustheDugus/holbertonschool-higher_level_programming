59574f991389635111af17b6c7766f1cea1223e6
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-04 19:09:20 -0500

    task 2

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let answer;
+if (process.argv.length < 3) {
+    answer = 'No argument';
+} else if (process.argv.length === 3) {
+    answer = 'Argument found';
+} else {
+    answer = 'Arguments found';
+}
+console.log(answer);
