0e29128f9a236ae512f2c9973122134e1373d049
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-04 20:35:30 -0500

    pushing to other machine

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+string = 'C is fun'
+x = parseInt.argv[2];
+if (x != parseInt.argv[2]){
+    console.log('Missing number of occurrences')
+}
+while (x > 0) {
+    console.log(string);
+    x--;
+}
