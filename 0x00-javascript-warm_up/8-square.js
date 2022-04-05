90b670555137a334958328b39475acf7b1ad0f29
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-04 20:50:52 -0500

    push to other machine

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+let x = process.argv[2];
+let y = x
+if (parseInt[x]) {
+    while (x > 0) { 
+        console.log('X')
+        x--
+        while (y > 0) {
+            console.log('X\n')
+            y--
+        }
+    }
+} else {
+    console.log('Missing size')
+}
