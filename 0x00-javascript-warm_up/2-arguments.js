7fd28adcf0c558428e3e92df9e97ab9e886ccffb
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-04 19:10:37 -0500

    semistandard task 2

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 let answer;
 if (process.argv.length < 3) {
-    answer = 'No argument';
+  answer = 'No argument';
 } else if (process.argv.length === 3) {
-    answer = 'Argument found';
+  answer = 'Argument found';
 } else {
-    answer = 'Arguments found';
+  answer = 'Arguments found';
 }
 console.log(answer);
