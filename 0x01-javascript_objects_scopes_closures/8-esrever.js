47269b0e43b8571cd852ffb0e8a1b18211f6d47c
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-05 22:10:03 -0500

    semistandard task 8

--- 0x01-javascript_objects_scopes_closures/8-esrever.js
+++ 0x01-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let j = list.length - 1;
-    const revlist = [];
-    for (let i = 0; i < list.length; i++) {
-      revlist[i] = list[j];
-      j--;
-    }
-    return revlist;
-  };
+  let j = list.length - 1;
+  const revlist = [];
+  for (let i = 0; i < list.length; i++) {
+    revlist[i] = list[j];
+    j--;
+  }
+  return revlist;
+};
