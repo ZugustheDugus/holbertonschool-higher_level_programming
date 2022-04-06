3f867f50b3fefbe90dc43d2b5ac8eec3ed5298ac
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-05 21:28:36 -0500

    move to other machine

---
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+    constructor (size) {
+        super(size, size)
+}
+
+charPrint (chr = 'X') {
+    for (let i = 0; i < this.width; i++) {
+        console.log(chr.repeat(this.width));
+    }
+    }
+};
