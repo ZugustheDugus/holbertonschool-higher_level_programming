5571f367ff847775ddc581461f01a3bf26e8d49b
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-07 15:10:13 -0500

    semistandard task 1

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fileserver = require('fs');
 fileserver.writefile(process.argv[2], process.argv[3],
-    'utf-8', function (error, content) {
-        if (error) {
-            console.error(error)
-        }
-    });
+  'utf-8', function (error, content) {
+    if (error) {
+      console.error(error);
+    }
+  });
