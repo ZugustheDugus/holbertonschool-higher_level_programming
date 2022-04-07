e03cfde038a0adfdf1748b93f9a6e94ff1baab66
ZugustheDugus <h1cooper2019@protonmail.com>
2022-04-07 17:13:45 -0500

    adfgasdfgsda

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fileserver = require('fs');
-fileserver.writefile(process.argv[2], process.argv[3],
+fileserver.writeFile(process.argv[2], process.argv[3],
   'utf-8', function (error, content) {
     if (error) {
       console.error(error);
